44dfcea9c66519adfe3fd365aabf27bcc7c001dd
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-08-17 15:36:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ function destructivelyRemoveFirstCat(){
     return 'cats';
 }
 function appendCat(){
-    var cats_1= [...cats,'Broom'];
+    const cats_1= [...cats,'Broom'];
     return cats_1;
 }
 function prependCat(){
-    var cats_2=['Arnold', ...cats];
+    const cats_2=['Arnold', ...cats];
     return cats_2;
   }
 function removeLastCat(){
-    var cats_3= cats.slice(0,cats.length-1);
+    const cats_3= cats.slice(0,cats.length-1);
     return cats_3;
   }
   function removeFirstCat(){
-    var cats_4= cats.slice(1)
+    const cats_4= cats.slice(1)
     return cats_4;
   }
